89a0d57ab2a6f0dedf71a212ae5cee3c3c70717f
CharlesOder <charlie.oder@gmail.com>
2019-07-03 13:28:51 -0500

    auto-closing side menu on clicks

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,24 @@
             <div class="nav-button close-button" @click="closeMenu()">
                 <a href="#">Close</a>
             </div>
-            <router-link class="nav-button" to="/characters">Character Setup</router-link>
-            <router-link class="nav-button" to="/gear-to-farm">Gear to Farm</router-link>
-            <router-link class="nav-button" to="/locations-to-farm">Locations to Farm</router-link>
-            <router-link class="nav-button" to="/gear-on-hand">Gear On Hand</router-link>
-            <router-link class="nav-button" to="/gear-needed-per-character">Gear Needed/Character</router-link>
-            <router-link class="nav-button" to="/gear-needed-total">Total Gear Needed</router-link>
+            <router-link class="nav-button"
+                         @click.native="closeMenu()"
+                         to="/characters">Character Setup</router-link>
+            <router-link class="nav-button"
+                         @click.native="closeMenu()"
+                         to="/gear-to-farm">Gear to Farm</router-link>
+            <router-link class="nav-button"
+                         @click.native="closeMenu()"
+                         to="/locations-to-farm">Locations to Farm</router-link>
+            <router-link class="nav-button"
+                         @click.native="closeMenu()"
+                         to="/gear-on-hand">Gear On Hand</router-link>
+            <router-link class="nav-button"
+                         @click.native="closeMenu()"
+                         to="/gear-needed-per-character">Gear Needed/Character</router-link>
+            <router-link class="nav-button"
+                         @click.native="closeMenu()"
+                         to="/gear-needed-total">Total Gear Needed</router-link>
             <div class="nav-button" @click="fetchCharacterData()">
                 <a href="#">Fetch Character Data</a>
             </div>
@@ -84,6 +96,7 @@
                 ' will overwrite any existing local data. (May require enabling popups)'
                 : 'Are you sure you want to turn off automatic cloud sync?';
             AlertBus.showDialog(message, 'Yes', () => {
+                this.closeMenu();
                 this.setAutoSave(autoSave);
                 if (autoSave) {
                     SetupStateManager.shared.pullDataFromCloud();
@@ -94,7 +107,8 @@
         private confirmSaveToCloud() {
             AlertBus.showDialog('Are you sure you want to overwrite your cloud data? (May require enabling popups)',
                 'Yes', () => {
-                SetupStateManager.shared.saveDataToCloudAndWait().then(() => {
+                    this.closeMenu();
+                    SetupStateManager.shared.saveDataToCloudAndWait().then(() => {
                     AlertBus.alertMessage('Data Saved');
                 });
             });
@@ -103,7 +117,8 @@
         private confirmPullFromCloud() {
             AlertBus.showDialog('Are you sure you want to overwrite your local data? (May require enabling popups)' ,
                 'Yes', () => {
-                SetupStateManager.shared.pullDataFromCloudAndWait().then(() => {
+                    this.closeMenu();
+                    SetupStateManager.shared.pullDataFromCloudAndWait().then(() => {
                     AlertBus.alertMessage('Data Loaded');
                 });
             });
